c313dfd55536c09558affae69d9ce86c86b16005
tangboo <13164286167@163.com>
2018-11-02 18:49:24 +0800

    wx test1

---
+++ wx/wxkf/pages/login/login.js
@@ -0,0 +1,39 @@
+// pages/login/login.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    username: '',
+    userpwd: ''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    back:function(){
+      wx.navigateTo({
+        url: '../index/index',
+      })
+    },
+    btnLogin:function(){
+      if(this.username === 'tangbo' && this.userpwd === '123'){
+        console.log('登陆成功...');
+        wx.navigateTo({
+          url: '../user/user',
+        })
+      }else{
+        console.log(this.username)
+        console.log('用户名或密码错误！请重新输入！')
+      }
+    }
+  }
+})
