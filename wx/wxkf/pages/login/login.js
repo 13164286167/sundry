42d888cac3aeef4037eb458a7cf21dfed5330d85
tangboo <13164286167@163.com>
2018-11-05 17:12:16 +0800

    2

--- wx/wxkf/pages/login/login.js
+++ wx/wxkf/pages/login/login.js
@@ -6,45 +6,30 @@ Component({
   properties: {
 
   },
-
+  
   /**
    * 组件的初始数据
    */
   data: {
-    username: '',
-    userpwd: ''
+    comfire: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    
     back:function(){
       wx.navigateTo({
         url: '../index/index',
       })
     },
-    btnLogin:function(){
-      if(this.username === 'tangbo' && this.userpwd === '123'){
-        console.log('登陆成功...');
-        wx.navigateTo({
-          url: '../user/user',
-        })
-      }else{
-        console.log(this.username)
-        console.log('用户名或密码错误！请重新输入！')
-      }
-    },
-    forname: function(e){
+    btnClick: function(){
       this.setData({
-        username:e.detail.value
-      });
-    },
-    forpwd: function (e) {
-      console.log(e)
-      this.setData({
-        userpwd: e.detail.value
-      });
-    },
+        comfire: true
+      }),
+      console.log(wx.getStorageSync('2'))
+     
+    }
   }
 })
