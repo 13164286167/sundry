9ad29f51ff87ef16e883ec2f8c77a3e2be0ec3e7
tangbo2 <13164286167@163.com>
2019-08-12 17:26:17 +0800

    echart

---
+++ echarts/myproject/src/components/Test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div ref="test">1</div>
+    {{this.test.text}}
+  </div>
+</template>
+<script>
+export default {
+  name: "test",
+  props: ["test", "test2", "test3"],
+  methods: {
+    handleDiv() {
+      this.$refs.test.innerHTML = this.test3.a;
+    }
+  },
+  watch: {
+    test(newValue) {
+      console.log(newValue);
+    },
+    test2: {
+      //深度监听，可监听到对象、数组的变化
+      handler(newV, oldV) {
+        // do something, 可使用this
+        console.log(newV, oldV);
+      },
+      deep: true
+    }
+  },
+  updated() {
+    console.log("update--test2", this.test2);
+    this.handleDiv();
+  },
+  mounted() {
+    console.log(this.test2);
+    console.log(this.test3);
+    this.handleDiv();
+  }
+};
+</script>
--- echarts/myproject/src/views/Home.vue
+++ echarts/myproject/src/views/Home.vue
@@ -1,62 +1,85 @@
 <template>
   <div class="home">
     <div id="echarts">
-      <Layout @close="alertClose" v-if="!close"></Layout>
+      {{testData.text}}
+      <br />以下是test组件
+      <br />--------------
+      <br />
+
+      <Test :test3="isChange" :test="testData" :test2="testArr"></Test>
+
+      <button @click="changeData">更改数据</button>
+      <!-- <Layout @close="alertClose" v-if="!close"></Layout> -->
       <!-- <Echarts v-if="show[0]" :edata='homeData'></Echarts>
       <RadarEchart v-else-if="show[1]"></RadarEchart>
-      <button @click='handleClick'>change</button> -->
+      <button @click='handleClick'>change</button>-->
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-console.log(store)
-import Echarts from '@/components/Echarts'
-import Layout from '@/components/Layout'
+console.log(store);
+import Echarts from "@/components/Echarts";
+import Layout from "@/components/Layout";
+import Test from "@/components/Test";
 
-import TimeEchart from '@/components/TimeEchart'
-import RadarEchart from '@/components/RadarEchart'
+import TimeEchart from "@/components/TimeEchart";
+import RadarEchart from "@/components/RadarEchart";
 export default {
-  name: 'home',
-  data(){
-    return{
-      show:[false,true],
-      close:false
-    }
+  name: "home",
+  data() {
+    return {
+      show: [false, true],
+      close: false,
+      testData: {
+        text: "这是home里的数据"
+      },
+      testArr: [{}],
+      index: 0,
+      isChange: { a: "1" }
+    };
   },
   components: {
     Echarts,
     TimeEchart,
     RadarEchart,
-    Layout
+    Layout,
+    Test
   },
-  computed:{
-    homeData(){
-      return store.state.homeData
+
+  computed: {
+    homeData() {
+      return store.state.homeData;
+    }
+  },
+  methods: {
+    changeData() {
+      this.index++;
+      this.isChange.a = this.isChange.a + 1;
+      let data = {data:[1,2,this.index]}
+      this.testArr.splice(0,1,{data})
+      // this.testData.text = "这是更改后的数据来自home"+this.index
     },
-    
-  },
-  methods:{
-    handleClick(){
+    handleClick() {
       // store.dispatch("CHANGEHOMEDATA")
-      console.log('执行')
-      this.show.forEach((element,index  ) => {
+      console.log("执行");
+      this.show.forEach((element, index) => {
         this.show[index] = !element;
       });
     },
-    alertClose(){
+    alertClose() {
       this.close = true;
     }
   }
-}
+};
 </script>
 <style>
- #echarts{
-   /* 给边框多给了4 */
-   width: 825px;  
-   height: 261px;
-   box-sizing: border-box;
-   /* border:1px solid red; */
-  }
+#echarts {
+  /* 给边框多给了4 */
+  width: 825px;
+  height: 261px;
+  box-sizing: border-box;
+  /* border:1px solid red; */
+}
 </style>
